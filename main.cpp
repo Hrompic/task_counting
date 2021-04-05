4ccfa8a7929939a296676c5aa69307e4a4750688
Hrompic <semenxx@gmail.com>
2021-04-05 16:08:48 +0300

    Update

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 int main()
 {
 	cout << "Hello World!" << endl;
 	string input;
+	string word;
 	while(getline(cin, input))
 	{
-		cout <<input;
+		stringstream ss(input);
+
+		while(ss >>word)
+			cout <<word <<endl;
 	}
 	return 0;
 }
